94f0d44247532f617acdb8dc9f8f68c1d83d6f7a
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-04-25 12:15:40 +0100

    Update main2.js

--- server/main2.js
+++ server/main2.js
@@ -7,7 +7,7 @@ const db = await mysql.createConnection({
     user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE,
-    port:process.env.PORT
+    port:process.env.PORTE
 })
 if(db){
     console.log("✅ Quiz database opened")
