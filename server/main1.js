2f23e29d5bfe2a8e9cbc178df41f9ff858a2e799
ayoolaowoilu <ayoolaowoilu@gmail.com>
2025-04-25 12:20:28 +0100

    Update main1.js

--- server/main1.js
+++ server/main1.js
@@ -11,7 +11,7 @@ const db = await mysql.createConnection({
     user:process.env.USER,
     password:process.env.PASSWORD,
     database:process.env.DATABASE,
-    port:process.env.PORT
+    port:process.env.PORTE
 })
 if(db){
     console.log("✅Database connected ")
